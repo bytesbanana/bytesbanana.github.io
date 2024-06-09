5c55b7beb84f33bf38dbe99182e4008879ed35f7
bytesbanana <saharat.paynok@gmail.com>
2024-06-09 13:36:14 +0700

    move hero section into component

---
+++ src/components/hero-section.tsx
@@ -0,0 +1,61 @@
+import { motion } from "framer-motion";
+import { title } from "./primitives";
+import { cn } from "@nextui-org/theme";
+import { ComponentProps } from "react";
+
+export const HeroSection = ({
+  className,
+  ...rest
+}: ComponentProps<"section">) => {
+  return (
+    <section
+      className={cn(
+        "flex flex-col items-center justify-center gap-4 py-8 md:py-10 min-h-[calc(50dvh)]",
+        className
+      )}
+      {...rest}
+    >
+      <div className="flex flex-col items-center justify-center gap-2 text-center">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            marginTop: "-1rem",
+          }}
+          className={title()}
+          animate={{ opacity: 1, scale: 1, marginTop: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <span className="animate-wave"> 🖖🏽 &nbsp;</span>Hi there!&nbsp;
+        </motion.h1>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            marginTop: "-1rem",
+          }}
+          animate={{ opacity: 1, scale: 1, marginTop: 0 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          className={cn(title())}
+        >
+          {"I'm "}&nbsp;
+          <span className={cn(title({ color: "yellow" }), "drop-shadow-xl")}>
+            {"Saharat Paynok"}
+          </span>
+        </motion.h1>
+        <motion.h2
+          initial={{
+            opacity: 0,
+            marginTop: "-1rem",
+          }}
+          animate={{ opacity: 1, scale: 1, marginTop: 0 }}
+          transition={{ duration: 0.5, delay: 1.2 }}
+          className={title({
+            size: "sm",
+            fullWidth: true,
+          })}
+        >
+          A software developer based in Thailand 🇹🇭
+        </motion.h2>
+      </div>
+    </section>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,10 @@
-import { cn } from "@nextui-org/theme";
-
-import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
-import { motion } from "framer-motion";
+import { HeroSection } from "@/components/hero-section";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="flex flex-col items-center justify-center gap-2 text-center">
-          <motion.h1
-            initial={{
-              opacity: 0,
-              marginTop: "-1rem",
-            }}
-            className={title()}
-            animate={{ opacity: 1, scale: 1, marginTop: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <span className="animate-wave"> 🖖🏽 &nbsp;</span>Hi there!&nbsp;
-          </motion.h1>
-          <motion.h1
-            initial={{
-              opacity: 0,
-              marginTop: "-1rem",
-            }}
-            animate={{ opacity: 1, scale: 1, marginTop: 0 }}
-            transition={{ duration: 0.5, delay: 0.6 }}
-            className={cn(title())}
-          >
-            {"I'm "}&nbsp;
-            <span className={cn(title({ color: "yellow" }), "drop-shadow-xl")}>
-              {"Saharat Paynok"}
-            </span>
-          </motion.h1>
-          <motion.h2
-            initial={{
-              opacity: 0,
-              marginTop: "-1rem",
-            }}
-            animate={{ opacity: 1, scale: 1, marginTop: 0 }}
-            transition={{ duration: 0.5, delay: 1.2 }}
-            className={title({
-              size: "sm",
-              fullWidth: true,
-            })}
-          >
-            A software developer based in Thailand 🇹🇭
-          </motion.h2>
-        </div>
-      </section>
+      <HeroSection />
     </DefaultLayout>
   );
 }
