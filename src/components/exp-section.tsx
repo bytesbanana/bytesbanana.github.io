75582df5fbf699f367e925f804d00a3170e6682b
bytesbanana <saharat.paynok@gmail.com>
2024-06-13 00:20:30 +0700

    add exp details

--- src/components/exp-section.tsx
+++ src/components/exp-section.tsx
@@ -22,7 +22,56 @@ export const ExpSection = () => {
         Experiences
       </motion.h1>
 
-      <ExpTimeline />
+      <ExpTimeline
+        expInfos={[
+          {
+            year: "Jun 2018 - Mar 2020",
+            position: "Programmer analyst",
+            companyName: "ALPHAMETRICS CO., LTD., Saraburi, Thailand",
+            description: `- Created an API for user licensing control using ASP.NET to assist the business in billing more than 25% of customers.
+- Implement a Microsoft dynamic extension using AL language for Point of Sales that allows clients to interact with the system using a barcode scanner.
+- Created a Java-based custom JIRA extension to make it simple for the team to get a custom report in an Excel-based format.
+- Participate in research and proof of concepts to improve the company's products`,
+          },
+          {
+            year: "Mar 2020 - Apr 2021",
+            position: "Java Programmer (Contract)",
+            companyName: "SOLUTION LINK, Bangkok, Thailand",
+            description: `- Created a full-stack web application utilizing Java (1.6), Spring Framework MVC, JavaScript, Bootstrap CSS, and JSP to manage each customer's SMS.
+- Developed a full-stack web application to automatically extract, transform, and load insurance data from the company's partner to simplify the insurance issue process.`,
+          },
+          {
+            year: "Apr 2021 - Jun 2022",
+            position: "Frontend Developer",
+            companyName: "SOLUTION LINK, Bangkok, Thailand",
+            description: `- Working closely with product owners while utilizing agile methodology
+- Examine product problems and bug fixes.
+- Implement the NDID feature with JavaScript and Kony to assist clients in simplifying
+identification and permission without contacting any bank branches.
+- Use Next.JS, ReduxJS, Axios, and Styled-Components to implement, test, debug,
+and deploy web-view-based mobile banking capabilities such as international transfers, online account opening, and E-KYC.`,
+          },
+          {
+            year: "Jul 2022 - Dec 2023",
+            position: "Senior Programmer (Contract)",
+            companyName: "ADI GROUP, Bangkok, Thailand",
+            description: `- Worked with a small agile team to scope and prioritize features.
+- Wrote technical design documents, including sequence diagrams and ER diagrams,
+after analyzing and designing APIs from specifications.
+- Utilized technical expertise to produce Rest API, unit test scripts using Spring Boot,
+Postgres, and Redis, and Karate test automation scripts.
+- Use JMeter to load tests and compile the results into a report.`,
+          },
+          {
+            year: "Feb 2023 - Present",
+            position: "Senior Software Engineer",
+            companyName:
+              "REFINITIV SOFTWARE (THAILAND) COMPANY LIMITED, Bangkok, Thailand",
+            description: `- Implement and resolve the issue with the high-performance chart application, which is used by more than 10,000 customers per day, using VueJS, React, Redux, Redux- Saga, and Typescript.
+- Collaborated with a global agile team of over 20 people to help with scope and feature prioritization.`,
+          },
+        ]}
+      />
     </section>
   );
 };
--- src/components/exp-timeline.tsx
+++ src/components/exp-timeline.tsx
@@ -1,12 +1,27 @@
 import { cn } from "@nextui-org/theme";
 import { motion } from "framer-motion";
+import { ComponentProps } from "react";
+import Markdown from "markdown-to-jsx";
 
-const ExpNode = (
-  { position }: { position: "left" | "right" } = { position: "right" }
-) => {
-  console.log(position);
+type ExpInfo = {
+  year: string;
+  position: string;
+  description: string;
+  companyName: string;
+};
+
+interface ExpTimelineProps extends ComponentProps<"section"> {
+  expInfos: Array<ExpInfo>;
+}
+
+interface ExpNodeProps extends ComponentProps<"div"> {
+  position: "left" | "right";
+  expInfo: ExpInfo;
+}
+
+const ExpNode = ({ expInfo, position }: ExpNodeProps) => {
   return (
-    <div className="relative ">
+    <div className="relative">
       <div className="flex items-center justify-center w-10 h-10 bg-green-500 rounded-full shadow-sm">
         <div className="w-5 h-5 bg-white rounded-full shadow-md animate-pulse"></div>
       </div>
@@ -23,7 +38,7 @@ const ExpNode = (
             position === "left" && "text-end"
           )}
         >
-          Year
+          {expInfo.year}
         </h3>
         <div className="relative">
           <div
@@ -46,35 +61,36 @@ const ExpNode = (
             position === "left" && "text-end"
           )}
         >
-          Position
+          {expInfo.position}
         </h4>
-        <p className={cn(position === "left" && "text-end")}>description</p>
+        <Markdown options={{
+          overrides: {
+            ul: {
+              props: {
+                className: 'flex flex-col gap-2'
+              }
+            },
+            li: {
+              props: {
+                className: 'list-disc ml-6'
+              }
+            }
+          }
+        }}>{expInfo.description.trim()}</Markdown>
+        <p className={cn(position === "left" && "text-end")}></p>
       </div>
     </div>
   );
 };
 
 const ExpLine = () => {
-  return <div className="w-2 h-[200px] bg-green-500"></div>;
+  return <div className="w-2 h-[250px] bg-green-500" />;
 };
 
-const ExpTimeline = () => {
-  const renderExpNode = () => {
-    const nodes: React.ReactNode[] = [];
-
-    for (let i = 0; i < 5; i++) {
-      nodes.push(<ExpNode position={i % 2 === 0 ? "right" : "left"} />);
-      if (i < 4) {
-        nodes.push(<ExpLine />);
-      }
-    }
-
-    return nodes;
-  };
-
+const ExpTimeline = ({ expInfos, className }: ExpTimelineProps) => {
   return (
     <motion.section
-      className="flex flex-col items-center"
+      className={cn("flex flex-col items-center", className)}
       initial={{
         opacity: 0,
         marginTop: "-1rem",
@@ -86,16 +102,15 @@ const ExpTimeline = () => {
         once: true,
       }}
     >
-      {renderExpNode()}
-      {/* <ExpNode />
-      <ExpLine />
-      <ExpNode />
-      <ExpLine />
-      <ExpNode />
-      <ExpLine />
-      <ExpNode />
-      <ExpLine />
-      <ExpNode /> */}
+      {expInfos.map((expInfo, i) => (
+        <>
+          <ExpNode
+            expInfo={expInfo}
+            position={i % 2 === 0 ? "right" : "left"}
+          />
+          {i < expInfos.length - 1 && <ExpLine />}
+        </>
+      ))}
     </motion.section>
   );
 };
